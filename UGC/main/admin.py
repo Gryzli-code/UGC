f7064aee7eec1322da3b0605099f5ed8ef0716d9
Gryzli-code <89235173397@mail.ru>
2026-07-25 11:39:41 +0300

    настройка, создание модели CustomUser

---
+++ UGC/main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUser(admin.ModelAdmin):
+    list_display = ("username", "role")
+    list_editable = ("role",)
+    search_fields = ("username",)
